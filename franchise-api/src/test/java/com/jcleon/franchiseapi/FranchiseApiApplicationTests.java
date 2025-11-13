1fe153f2b2d822986f2cbc8b96bde8610cc31ff1
julioLeon892 <jcleon892@gmail.com>
2025-11-13 11:15:40 -0500

    chore: inicializar proyecto Spring Boot backend

---
+++ franchise-api/src/test/java/com/jcleon/franchiseapi/FranchiseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcleon.franchiseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FranchiseApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
